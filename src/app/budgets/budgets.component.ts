8952a8437954cee08f917042dcb7815c1c2f1bd2
unknown <nicholasghob@yahoo.com>
2018-04-22 20:25:22 -0400

    Added functionality for adding and removing budgets. Removed checklist page

--- src/app/budgets/budgets.component.ts
+++ src/app/budgets/budgets.component.ts
@@ -6,6 +6,8 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireObject } from 'angularfire2/database/interfaces';
 
+import { AngularFireList } from 'angularfire2/database';
+
 @Component({
   selector: 'app-budgets',
   templateUrl: './budgets.component.html',
@@ -26,9 +28,14 @@ export class BudgetsComponent implements OnInit {
   textboxText: string = '';
 
   budgetsObservable: Observable<any[]>;
+  budgetsRef: AngularFireList<any>;
 
   constructor(private _data: DataService, private db: AngularFireDatabase) {
-    this.budgetsObservable = db.list('/budgets').valueChanges();
+    this.budgetsRef = db.list('/budgets');
+    this.budgetsObservable = this.budgetsRef.snapshotChanges().map(
+      changes => {
+          return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+      });
    }
 
   ngOnInit() {
@@ -36,8 +43,12 @@ export class BudgetsComponent implements OnInit {
 
   addItem() {
     var newBudget = { name: this.textboxText  };
-    this.db.database.ref('/budgets').push(newBudget);
+    this.budgetsRef.push(newBudget);
     this.textboxText = '';
+  }
+
+  removeItem(key: string) {
+    this.budgetsRef.remove(key);
   }
 
 
